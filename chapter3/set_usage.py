374b6a0e549c3dc03a0a5ef56cd948436e619c3c
vicky_li <xinze.li1@pacter.com>
2021-04-08 09:33:21 +0800

    update chapter3

--- chapter3/set_usage.py
+++
@@ -1,5 +0,0 @@
-# encoding:utf-8
-# @date:
-# @author: vicky
-# @environment:
-# @brief:
--- chapter_3/dict_usage.py
+++ chapter_3/dict_usage.py
@@ -26,27 +26,65 @@ print(country_code)
 # .get(k) method could return a none
 print(country_code.get("CHINA"))
 # country_code[key] will return a error if there is no key
-print(country_code["CHINA"])
+# print(country_code["CHINA"])
 # 3-2
 # use setdefault to deal with situation where you can not find the key
-import sys
-import re
 
-WORD_RE = re.compile(r"\w+")
-index = {}
-with open(sys.argv[1], encoding='utf-8') as file:
-    for line_no, line in enumerate(file, 1):
-        for match in WORD_RE.finditer(line):
-            word = match.group()
-            column_no = match.start() +1
-            location = (line_no, line)
 
-            occurrence = index.get(word, [])
-            occurrence.append(location)
-            index[word] = occurrence
+def setdefault_fun():
+    """setdefault"""
+    import sys
+    import re
 
-for word in sorted(index, key=str.upper()):
-    print(word, index[word])
+    WORD_RE = re.compile(r"\w+")
+    index = {}
+    file_name = sys.argv[1]  # run$python dict_usage.py filename.txt
+    with open(file_name, encoding='utf-8') as file:
+        for line_no, line in enumerate(file, 1):
+            for match in WORD_RE.finditer(line):
+                word = match.group()
+                column_no = match.start() + 1
+                location = (line_no, line)
+                # following three rows could be replace  by a better method(dict.setdefault)
+                occurrence = index.get(word, [])
+                occurrence.append(location)
+                index[word] = occurrence
+                #
+                index.setdefault(word, []).append(location)
+
+    for word in sorted(index, key=str.upper):
+        print(word, index[word])
+
+
+def defaultdict_fun():
+    """method of defaultdict"""
+    # data mining usually use this method
+    import sys
+    import re
+    import collections
+
+    WORD_RE = re.compile(r"\w+")
+    index = collections.defaultdict(list)
+    # file_name = sys.argv[1]  # run$python dict_usage.py filename.txt
+    file_name = "test_dict.txt"
+    with open(file_name, encoding='utf-8') as file:
+        for line_no, line in enumerate(file, 1):
+            for match in WORD_RE.finditer(line):
+                word = match.group()
+                column_no = match.start() + 1
+                location = (line_no, line)
+                #
+                index[word].append(location)
+
+    for word in sorted(index, key=str.upper):
+        print(word, index[word])
+
+
+if __name__ == "__main__":
+    # setdefault_fun()
+    defaultdict_fun()
+
+
 
 
 
