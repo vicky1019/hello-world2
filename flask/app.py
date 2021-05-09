8eb7afc65a627220c89dd67aa3b4a001e3cd68e7
pactera-vicky <li.xin.ze@163.com>
2021-05-09 17:23:17 +0800

    api update

--- flask/app.py
+++ flask/app.py
@@ -4,14 +4,23 @@
 # @environment:
 # @brief: a simple instance about how to create a http api interface with python
 
+
 from flask import Flask
 from flask import jsonify
 
 app = Flask(__name__)
 
+# two ways of your requests(methods=)
+# 1.get
+# 2.post
+# the different between "GET" and "POST"
+# "GET": you can see the parameters in the URL,and the max is 1024 byte
+# "POST": no byte limited, and date transfer by forms,usually file uploads and content includes passwords etc.
 @app.route("/", methods=["GET"])
 def hello():
     return jsonify({"msg": "you are brilliant!"})
+
+
 
 # start server
 app.run(host="0.0.0.0", port=8000, debug=True)
